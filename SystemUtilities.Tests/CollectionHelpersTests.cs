a8f639de62fb3a32482d21bd52bee5a8331f4242
Ronald Rink <ronald.rink@d-fens.net>
2015-07-03 09:31:23 +0200

    added CollectionsHelpers

---
+++ SystemUtilities.Tests/CollectionHelpersTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Specialized;
+using biz.dfch.CS.System.Utilities;
+
+namespace biz.dfch.CS.System.Utilties.Tests
+{
+    [TestClass]
+    public class CollectionHelpersTests
+    {
+        [TestMethod]
+        public void EqualCollectionsShouldReturnTrue()
+        {
+            // Arrange
+            var left = new NameValueCollection();
+            left.Add("arbitrary-name1", "arbitrary-value1");
+            left.Add("arbitrary-name2", "arbitrary-value2");
+
+            var right = new NameValueCollection();
+            right.Add("arbitrary-name1", "arbitrary-value1");
+            right.Add("arbitrary-name2", "arbitrary-value2");
+
+            // Act
+            var fReturnExactOrder = CollectionHelpers.CompareNameValueCollections(left, right, true);
+            var fReturnAnyOrder = CollectionHelpers.CompareNameValueCollections(left, right, false);
+
+            // Assert
+            Assert.IsTrue(fReturnExactOrder);
+            Assert.IsTrue(fReturnAnyOrder);
+        }
+        [TestMethod]
+        public void EqualNotExactCollectionsShouldReturnFalse()
+        {
+            // Arrange
+            var left = new NameValueCollection();
+            left.Add("arbitrary-name1", "arbitrary-value1");
+            left.Add("arbitrary-name2", "arbitrary-value2");
+
+            var right = new NameValueCollection();
+            right.Add("arbitrary-name2", "arbitrary-value2");
+            right.Add("arbitrary-name1", "arbitrary-value1");
+
+            // Act
+            var fReturnExactOrder = CollectionHelpers.CompareNameValueCollections(left, right, true);
+            var fReturnAnyOrder = CollectionHelpers.CompareNameValueCollections(left, right, false);
+
+            // Assert
+            Assert.IsFalse(fReturnExactOrder);
+            Assert.IsTrue(fReturnAnyOrder);
+        }
+        [TestMethod]
+        public void NotEqualCollectionsShouldReturnFalse()
+        {
+            // Arrange
+            var left = new NameValueCollection();
+            left.Add("arbitrary-name1", "arbitrary-value1");
+            left.Add("arbitrary-name2", "arbitrary-value2");
+
+            var right = new NameValueCollection();
+            right.Add("arbitrary-name1", "arbitrary-value-slightly-different");
+            right.Add("arbitrary-name2", "arbitrary-value2");
+
+            // Act
+            var fReturnExactOrder = CollectionHelpers.CompareNameValueCollections(left, right, true);
+            var fReturnAnyOrder = CollectionHelpers.CompareNameValueCollections(left, right, false);
+
+            // Assert
+            Assert.IsFalse(fReturnExactOrder);
+            Assert.IsFalse(fReturnAnyOrder);
+        }
+        [TestMethod]
+        public void EmptyCollectionsShouldReturnTrue()
+        {
+            // Arrange
+            var left = new NameValueCollection();
+            var right = new NameValueCollection();
+
+            // Act
+            var fReturnExactOrder = CollectionHelpers.CompareNameValueCollections(left, right, true);
+            var fReturnAnyOrder = CollectionHelpers.CompareNameValueCollections(left, right, false);
+
+            // Assert
+            Assert.IsTrue(fReturnExactOrder);
+            Assert.IsTrue(fReturnAnyOrder);
+        }
+        [TestMethod]
+        public void NullCollectionsShouldReturnFalse()
+        {
+            // Arrange
+            NameValueCollection left = null;
+            NameValueCollection right = null;
+
+            // Act
+            var fReturnExactOrder = CollectionHelpers.CompareNameValueCollections(left, right, true);
+            var fReturnAnyOrder = CollectionHelpers.CompareNameValueCollections(left, right, false);
+
+            // Assert
+            Assert.IsFalse(fReturnExactOrder);
+            Assert.IsFalse(fReturnAnyOrder);
+        }
+    }
+}
---
+++ SystemUtilities/CollectionHelpers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace biz.dfch.CS.System.Utilities
+{
+    public class CollectionHelpers
+    {
+        public static bool CompareNameValueCollections
+            (
+            NameValueCollection left
+            ,
+            NameValueCollection right
+            ,
+            bool exactOrder
+            )
+        {
+            if(null == left || null == right)
+            {
+                return false;
+            }
+            var leftKeys = left.AllKeys;
+            var rightKeys = right.AllKeys;
+            if (leftKeys.Count() != rightKeys.Count())
+            {
+                return false;
+            }
+            if (exactOrder)
+            {
+                return leftKeys
+                        .SequenceEqual(rightKeys)
+                            && leftKeys.All(key => left[key] == right[key]);
+            }
+            else
+            {
+                return leftKeys
+                    .OrderBy(key => key)
+                        .SequenceEqual(rightKeys.OrderBy(key => key))
+                            && leftKeys.All(key => left[key] == right[key]);
+            }
+        }
+    }
+}
