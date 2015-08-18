841a4a7aa24138beceee895f75e4972732a19ebc
rufer7 <marc.rufer@d-fens.net>
2015-08-18 14:08:33 +0200

    assembly titles adjsuted according d-fens naming convention

--- SystemUtilities.Tests/Properties/AssemblyInfo.cs
+++ SystemUtilities.Tests/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SystemUtilities.Tests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("biz.dfch.CS.System.Utilities.Tests")]
+[assembly: AssemblyDescription("Tests for CSharp System Utilities")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("d-fens GmbH")]
 [assembly: AssemblyProduct("SystemUtilities.Tests")]
--- SystemUtilities/Properties/AssemblyInfo.cs
+++ SystemUtilities/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SystemUtilities")]
+[assembly: AssemblyTitle("biz.dfch.CS.System.Utilities")]
 [assembly: AssemblyDescription("CSharp System Utilities")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("d-fens GmbH")]
