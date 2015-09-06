3d4694dc90256634273fbd47a93f1485d8ba3a30
Ronald Rink <ronald.rink@d-fens.net>
2015-09-06 21:18:08 +0200

    code move from Cumulus with adjustments for current project

---
+++ src/biz.dfch.CS.System.Utilities.Tests/Convert/JsonHelperTest.cs
@@ -0,0 +1,176 @@
+﻿/**
+ *
+ *
+ * Copyright 2014-2015 Ronald Rink, d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using biz.dfch.CS.Utilities.Convert;
+using System.Diagnostics;
+
+namespace biz.dfch.CS.Utilities.Tests.Convert
+{
+    [TestClass]
+    public class JsonHelperTest
+    {
+        [ClassInitialize()]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            Trace.WriteLine(String.Format("classInitialize: '{0}'", testContext.TestName));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void doToJsonNullThrowsArgumentNullException()
+        {
+            var deserialisedObject = JsonHelper.ToJson(null);
+            Assert.Fail("This test was expected to fail already in a previous assertion.");
+        }
+
+        [TestMethod]
+        public void doToJsonEmptyReturnsNull()
+        {
+            var deserialisedObject = JsonHelper.ToJson(String.Empty);
+            Assert.IsNull(deserialisedObject);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(JsonReaderException))]
+        public void doToJsonInvalidJsonStringThrowsJsonReaderException()
+        {
+            var deserialisedObject = JsonHelper.ToJson("invalid-json-string");
+            Assert.Fail("This test was expected to fail already in a previous assertion.");
+        }
+
+        [TestMethod]
+        public void doToJsonValidJsonStringReturnsObject()
+        {
+            var jsonString = @"
+                {
+                    'number':  42
+                    ,
+                    'string':  'string'
+                }";
+            var deserialisedObject = JsonHelper.ToJson(jsonString);
+            Assert.IsNotNull(deserialisedObject);
+
+            Assert.IsTrue(deserialisedObject.ContainsKey("number"));
+            Assert.IsInstanceOfType(deserialisedObject["number"], typeof(Int64));
+            Assert.AreEqual((Int64)42, deserialisedObject["number"]);
+
+            Assert.IsTrue(deserialisedObject.ContainsKey("string"));
+            Assert.IsInstanceOfType(deserialisedObject["string"], typeof(String));
+            Assert.AreEqual("string", deserialisedObject["string"]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void doParseNullThrowsArgumentNullException()
+        {
+            var deserialisedObject = JsonHelper.Parse(null);
+            Assert.Fail("This test was expected to fail already in a previous assertion.");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(JsonReaderException))]
+        public void doParseEmptyThrowsJsonReaderException()
+        {
+            var deserialisedObject = JsonHelper.Parse(String.Empty);
+            Assert.Fail("This test was expected to fail already in a previous assertion.");
+        }
+
+        [TestMethod]
+        public void doParseValidJsonStringReturnsObject()
+        {
+            var jsonString = @"
+                {
+                    'number':  42
+                    ,
+                    'string':  'string'
+                }";
+            var jsonObject = JsonHelper.Parse(jsonString);
+            Assert.IsNotNull(jsonObject);
+
+            var jtNumber = jsonObject.SelectToken("number", true);
+            Assert.IsNotNull(jtNumber);
+            Assert.AreEqual(42, jtNumber.Value<int>());
+
+            var jtString = jsonObject.SelectToken("string", true);
+            Assert.IsNotNull(jtString);
+            Assert.AreEqual("string", jtString.Value<string>());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(JsonReaderException))]
+        public void doParseInvalidJsonStringReturnsObject()
+        {
+            var jsonString = @"
+                {
+                    'number':  42
+
+                    'string':  'string'
+                }";
+            var jsonObject = JsonHelper.Parse(jsonString);
+        }
+
+        [TestMethod]
+        public void doFromJsonJTokenReturnsString()
+        {
+            var defaultValue = "<default value>";
+            var jsonString = @"
+                {
+                    'number':  42
+                    ,
+                    'string':  'string'
+                }";
+            var jsonObject = JsonHelper.Parse(jsonString);
+            Assert.IsNotNull(jsonObject);
+
+            var _string = JsonHelper.FromJson(jsonObject, "string", defaultValue);
+            Assert.AreEqual("string", _string);
+
+            var _number = JsonHelper.FromJson(jsonObject, "number", defaultValue);
+            Assert.AreEqual("42", _number);
+        }
+
+        [TestMethod]
+        public void doFromJsonJTokenReturnsDefaultValue()
+        {
+            var defaultValue = "<default value>";
+            var jsonString = @"
+                {
+                    'number':  42
+                    ,
+                    'string':  'string'
+                }";
+            var jsonObject = JsonHelper.Parse(jsonString);
+            Assert.IsNotNull(jsonObject);
+
+            var _string = JsonHelper.FromJson(jsonObject, "string-does-not-exist", defaultValue);
+            Assert.AreEqual(defaultValue, _string);
+        }
+
+        [TestMethod]
+        public void doEmptyStringReturnsObject()
+        {
+            var jsonString = JsonHelper.EmptyString();
+            Assert.IsNotNull(jsonString);
+            Assert.AreEqual("\"\"", jsonString);
+        }
+    }
+}
---
+++ src/biz.dfch.CS.System.Utilities/Convert/JsonHelper.cs
@@ -0,0 +1,148 @@
+﻿/**
+ *
+ *
+ * Copyright 2014-2015 Ronald Rink, d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Web.Script.Serialization;
+using System.Diagnostics;
+
+// http://stackoverflow.com/questions/3391936/using-webclient-for-json-serialization
+namespace biz.dfch.CS.Utilities.Convert
+{
+    class JsonHelper
+    {
+        public static string ToJson<T>(T instance)
+        {
+            string _return = String.Empty;
+            _return = new JavaScriptSerializer().Serialize(instance);
+            return _return;
+        }
+
+        public static string ToJson(Object instance)
+        {
+            string _return = String.Empty;
+            _return = new JavaScriptSerializer().Serialize(instance);
+            return _return;
+        }
+
+        public static Dictionary<String, Object> ToJson(String instance)
+        {
+            var _return = JsonConvert.DeserializeObject<Dictionary<String, Object>>(instance);
+            return _return;
+        }
+
+        public static JToken Parse(string instance)
+        {
+            return JObject.Parse(instance);
+        }
+
+        public static string FromJsonString(string json)
+        {
+            return JsonHelper.FromJson<string>(json);
+        }
+
+        public static T FromJson<T>(string json)
+        {
+            var serializer = new DataContractJsonSerializer(typeof(T));
+            using (var tempStream = new MemoryStream(Encoding.Unicode.GetBytes(json)))
+            {
+                return (T)serializer.ReadObject(tempStream);
+            }
+        }
+
+        public static string FromJson(Dictionary<String, Object> instance, string key, string defaultValue = "")
+        {
+            string _return = defaultValue;
+            try
+            {
+                if (instance.ContainsKey(key))
+                {
+                    //_return = instance[key].ToString();
+                    dynamic value = instance[key];
+                    if (value is Array && 0 <= value.Count)
+                    {
+                        _return = value[0];
+                    }
+                    else
+                    {
+                        _return = value.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(String.Format("FromJson: ERROR on instance with key '{0}'", key));
+                Debug.WriteLine(String.Format("{0}: {1}\r\n{2}", ex.Source, ex.Message, ex.StackTrace));
+                _return = defaultValue;
+            }
+            if (null == _return)
+            {
+                throw new ArgumentException(String.Format("Key '{0}' not found.", key));
+            }
+            return _return;
+        }
+
+        public static string FromJson(JToken instance, string key, string defaultValue = "")
+        {
+            string _return = defaultValue;
+            try
+            {
+                var jv = instance.SelectToken(key);
+                if (null != jv)
+                {
+                    _return = jv.ToString();
+                }
+                try
+                {
+                    var jArray = JToken.Parse(_return);
+                    if (jArray is JArray)
+                    {
+                        _return = jArray.ToObject<List<String>>()[0].ToString();
+                    }
+                }
+                catch
+                {
+                    // Intended not to catch this exception
+                    // If an exception is triggered, it just means we do not have a JArray
+                    // in which case we just continue with the string we already have.
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(String.Format("FromJson: ERROR on instance with key '{0}'", key));
+                Debug.WriteLine(String.Format("{0}: {1}\r\n{2}", ex.Source, ex.Message, ex.StackTrace));
+                _return = defaultValue;
+            }
+            if (null == _return)
+            {
+                throw new ArgumentException(String.Format("Key '{0}' not found.", key));
+            }
+            return _return;
+        }
+
+        public static string EmptyString()
+        {
+            return new JavaScriptSerializer().Serialize(String.Empty);
+        }
+    }
+}
