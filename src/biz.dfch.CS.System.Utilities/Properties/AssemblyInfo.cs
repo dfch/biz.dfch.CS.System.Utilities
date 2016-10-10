ba14c4745787b1bb0e90f6f1c576d5d48591b90c
Ronald Rink <ronald.rink@d-fens.net>
2016-10-10 07:42:58 +0200

    StrongName
    * added strong name to assemblies
    * updated InternalsVisibleTo

--- src/biz.dfch.CS.System.Utilities/Properties/AssemblyInfo.cs
+++ src/biz.dfch.CS.System.Utilities/Properties/AssemblyInfo.cs
@@ -34,4 +34,11 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.1.*")]
 
-[assembly: InternalsVisibleTo("biz.dfch.CS.System.Utilities.Tests")]
+[assembly: InternalsVisibleTo("biz.dfch.CS.System.Utilities.Tests" +
+",PublicKey=" +
+"00240000048000009400000006020000002400005253413100040000010001001b40be1e0fe173" +
+"6d8c862c055965cb78086432e7b2377bc9c36122936b394e789fcdfbfeecebbc1d7f90b0be7fd4" +
+"f871822cc3a06d5af4ff4d6a9fa0176b9b3fa53e17c407adeb1caec47ee243b213eff6fdb57438" +
+"a5677addf3b70bf0766b3db70773b39d7f9544146d4470c4d5f55d718680ba1da6f22dce4d5527" +
+"c8e202c0"
+)]
